848d41a8652da76b8fe9ba9d8383dec0844f73e4
jthro <jethro.rosette@outlook.com>
2023-12-13 17:26:48 +1100

    removed unnecessary files

--- learnrust/target/debug/deps/learnrust-24211cefcdc50999.d
+++
@@ -1,5 +0,0 @@
-/home/jthro/Documents/adventofcode/learnrust/target/debug/deps/liblearnrust-24211cefcdc50999.rmeta: src/main.rs
-
-/home/jthro/Documents/adventofcode/learnrust/target/debug/deps/learnrust-24211cefcdc50999.d: src/main.rs
-
-src/main.rs:
--- learnrust/target/debug/learnrust.d
+++
@@ -1,1 +0,0 @@
-/home/jthro/Documents/adventofcode/learnrust/target/debug/learnrust: /home/jthro/Documents/adventofcode/learnrust/src/main.rs
