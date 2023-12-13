c98d7a5f267391f3faf18e576c60a20e4da6534f
jthro <jethro.rosette@outlook.com>
2023-12-14 10:33:27 +1100

    Added working code for day 13 part 2

--- daythirteen/daythirteen.py
+++ daythirteen/daythirteen.py
@@ -9,15 +9,37 @@ for i,field in enumerate(input):
     for j,line in enumerate(field):
         k = 0
         mirror = True
-        while k < min(j, len(field)-j):                
-            print(j-k-1, j+k)
-            print(field[j-k-1])
-            print(field[j+k])
-            if field[j-k-1] != field[j+k]:
-                mirror = False
-                break
+        fuzzyUsed = False
+        while k < min(j, len(field)-j):
+            for l, characters in enumerate(line):
+                if field[j-k-1][l] != field[j+k][l]:
+                    if fuzzyUsed == False:
+                        fuzzyUsed = True
+                    else:
+                        mirror = False
+                        break
             k += 1
-        if mirror:
+        if mirror and fuzzyUsed:
             answers.append(j*100)
-        
-print(answers)
+
+for i, field in enumerate(input):
+    mirrorbar = 1
+    while mirrorbar < len(field[0]):
+        mirror = True
+        fuzzyUsed = False
+        for j, line in enumerate(field):
+            charactercounter = 0
+            while charactercounter < min(mirrorbar, len(field[j])-mirrorbar):
+                if line[mirrorbar-1-charactercounter] != line[mirrorbar+charactercounter]:
+                    if fuzzyUsed == False:
+                        fuzzyUsed = True
+                    else:
+                        mirror = False
+                        break
+                charactercounter += 1
+        if mirror and fuzzyUsed:
+            answers.append(mirrorbar)
+            break
+        mirrorbar += 1
+            
+print(sum(answers))        
